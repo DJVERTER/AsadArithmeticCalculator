50ce3fa9af032946de8625072115bb5c0489da0d
DJVERTER <creativekira1982@gmail.com>
2022-01-22 21:17:54 +0200

    added loop for continue

--- AsadArithmeticCalculator/Program.cs
+++ AsadArithmeticCalculator/Program.cs
@@ -27,48 +27,61 @@ namespace AsadArithmeticCalculator
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any following key to perform an arithmetic operation:");
-            Console.WriteLine();
-            Console.WriteLine("1 - Addition\n2 - Subtraction\n3 - Multipliation\n4 - Division");
-            Console.WriteLine();
-            int firtsNum;
-            int secondNum;
-            int result = 0;
-            int action = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter Value 1: ");
-            firtsNum = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Value 2: ");
-            secondNum = Convert.ToInt32(Console.ReadLine());
-
-            switch (action)
+            bool isContinue = true;
+            
+            while (isContinue)
             {
-                case 1:
-                    {
-                        result = Addition(firtsNum, secondNum);
-                        break;
-                    }
-                case 2:
-                    {
-                        result = Addition(firtsNum, secondNum);
-                        break;
-                    }
-                case 3:
-                    {
-                        result = Addition(firtsNum, secondNum);
-                        break;
-                    }
-                case 4:
-                    {
-                        result = Addition(firtsNum, secondNum);
-                        break;
-                    }
+                Console.WriteLine("Press any following key to perform an arithmetic operation:");
+                Console.WriteLine();
+                Console.WriteLine("1 - Addition\n2 - Subtraction\n3 - Multipliation\n4 - Division");
+                Console.WriteLine();
+                int firtsNum;
+                int secondNum;
+                int result = 0;
+                int action = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Value 1: ");
+                firtsNum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Value 2: ");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+
+                switch (action)
+                {
+                    case 1:
+                        {
+                            result = Addition(firtsNum, secondNum);
+                            break;
+                        }
+                    case 2:
+                        {
+                            result = Addition(firtsNum, secondNum);
+                            break;
+                        }
+                    case 3:
+                        {
+                            result = Addition(firtsNum, secondNum);
+                            break;
+                        }
+                    case 4:
+                        {
+                            result = Addition(firtsNum, secondNum);
+                            break;
+                        }
+                }
+
+                Console.WriteLine(result);
+                Console.WriteLine();
+
+                Console.WriteLine("Do you want to continue again (Y/N)?");
+                string end = Console.ReadLine();
+                if (end == "N")
+                {
+
+                    isContinue = false;
+                }
+                
             }
-
-            Console.WriteLine(result);
-            Console.WriteLine();
-
-            Console.WriteLine("Do you want to continue again (Y/N)?");
+            
         }
     }
 }
